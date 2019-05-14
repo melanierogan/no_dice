d671d717bfbccb2997720849063f067857d57ef3
Melanie Rogan <melanie.rogan@ft.com>
2019-05-14 13:53:08 +0100

    emperical evidence

--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -25,17 +25,34 @@
 
 require 'index'
 
+# I need to require the file i'm testing
+
+# I need to start a describe block that sets out the object i'm testing
 describe Dice do
+  # I want both of these elements to be available freshly each time a test runs
   let(:dice_roll) { rand(1..6) }
   let(:dice) { Dice.new }
-  
+
+  # I want to test that my dice gives me a number between one and six when rolled
   it 'should give a number between 1 and 6 when rolled' do
+    # It helped me to use the noun to specify what would happen when the dice is rolled, this is my arrange step
     result = dice_roll
+    # I then wanted to check that the result of that roll would be a random number between 1 and 6 inclusive
+    # I used the && operator to say that each needs to be true, so it should be greater than or equal to 1 but less than or equal to 6. 
+    # I expect that result to be true, so it is in a sense and act and assert in one go
     expect(result >= 1 && result <= 6) .to eq(true)
-  end
-
-  it 'should let me roll another dice' do
-
   end
 end
 
+# To sum up, I started with a class dice that wants to be able to roll in my requirements
+# I then wanted to feature test the existence of a class dice, that failed, so i wrote the matching unit test that also failed
+# Then i wrote code that created the class dice, this meant my test passed
+
+# I then needed to see that i had the ability to roll the dice, so dice.roll or dice_roll (as a variable)..that failed feature testing
+# I matched a unit test that failed, then wrote the code to pass by writing a roll method. 
+# I used respond_to(:dice_roll) to ensure the existence of that method within the class
+
+# I then looked at the requirements for the roll, it needed to come up with a number between 1 and 6 and it needed to be random.
+# I feature tested my def, it failed, my unit test failed, so I used rand(1..6), the test failed so I wrote the code to give me a random number rand(1..6), it passed
+
+# I then refactored to make dice_roll a global let that resets for each test
